ef12aa5adcc6849fbbf3185d80015bff7029ae40
Stoil Yankov <s.yankov16@gmail.com>
2025-06-27 18:31:15 +0300

    feat: Created background with animations, added theme toggle, added basic navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,58 @@
+import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+
+const navItems = [
+  { name: "Home", href: "#hero" },
+  { name: "About", href: "#about" },
+  { name: "Skills", href: "#skills" },
+  { name: "Projects", href: "#projects" },
+  { name: "Contact", href: "#contact" },
+];
+
+export const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.screenY > 10);
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <nav
+      className={cn(
+        "fixed w-full z-40 transition-all duration-300",
+        isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
+      )}
+    >
+      <div className="container flex items-center justify-between">
+        <a
+          href="#hero"
+          className="text-xl font-bold text-primary flex items-center"
+        >
+          <span className="relative z-10">
+            <span className="text-glow text-foreground"> Stoil Yankov </span>{" "}
+            Portfolio
+          </span>
+        </a>
+
+        {/* desktop nav */}
+        <div className="hidden md:flex space-x-8">
+          {navItems.map((item, key) => (
+            <a
+              key={key}
+              href={item.href}
+              className="text-foreground/80 hover:text-primary transition-colors duration-300"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+
+        {/* mobile nav */}
+      </div>
+    </nav>
+  );
+};
