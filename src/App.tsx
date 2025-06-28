f4928a34028064618fc91cb1eb73c389807bd878
Stoil Yankov <s.yankov16@gmail.com>
2025-06-28 09:22:11 +0300

    bugfix: Fix react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { NotFound } from "./pages/NotFound";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/stoilyankov.bg">
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
